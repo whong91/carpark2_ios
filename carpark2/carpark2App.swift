a320569683d45f8d028ab3f218e790f96d135a86
Wai Hong <waihong_lee@commontown.com>
2023-08-25 08:32:49 +0800

    Initial Commit

---
+++ carpark2/carpark2App.swift
@@ -0,0 +1,17 @@
+//
+//  carpark2App.swift
+//  carpark2
+//
+//  Created by Wai Hong Lee on 25/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct carpark2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
